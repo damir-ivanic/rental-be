de4737c86363b8c455da0b64fe6274a36b709817
Damir <damir.ivanic@advania.is>
2021-10-31 00:48:53 +0200

    Added sqllite initial models for user and post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Post } from './posts/post.entity';
+
+const config: TypeOrmModuleOptions = {
+  name: 'default',
+  type: 'sqlite',
+  database: 'db.sqlite3',
+  entities: [User, Post],
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    PostsModule,
+    VehiclesModule,
+    TypeOrmModule.forRoot(config),
+  ],
 })
 export class AppModule {}
